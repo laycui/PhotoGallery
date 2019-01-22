848316386a6fb0880ccc5d152bb784abac230c4f
Lei Cui <leicu@microsoft.com>
2019-01-21 21:31:19 -0800

    ImageLoader V2 - No handler

--- app/src/main/java/com/bignerdranch/android/photogallery/data/ImageManager.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/data/ImageManager.java
@@ -3,27 +3,24 @@ package com.bignerdranch.android.photogallery.data;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.Looper;
-import android.os.Message;
-import android.support.annotation.WorkerThread;
 import android.util.LruCache;
 
 import com.bignerdranch.android.photogallery.customview.LoadingImageView;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class ImageManager {
 
-  private static final String TAG = "ImageManager";
-
-  private Handler mRequestHandler;
   private Handler mUiHandler;
   private OkHttpClient mOkHttpClient;
   private ConcurrentMap<String, LoadingImageView> mUrl2Image;
@@ -39,24 +36,12 @@ public class ImageManager {
     return sImageManager;
   }
 
-  public ImageManager() {
+  private ImageManager() {
     mUrl2Image = new ConcurrentHashMap<>();
     mImage2Url = new ConcurrentHashMap<>();
     mUiHandler = new Handler(Looper.getMainLooper());
     mLruCache = new LruCache<>(100);
     mOkHttpClient = new OkHttpClient();
-    HandlerThread handlerThread = new HandlerThread(TAG);
-    handlerThread.start();
-    mRequestHandler =
-        new Handler(
-            handlerThread.getLooper(),
-            new Handler.Callback() {
-              @Override
-              public boolean handleMessage(Message message) {
-                handleRequest((String) message.obj);
-                return false;
-              }
-            });
   }
 
   public void bindImageWithUrl(String url, LoadingImageView loadingImageView) {
@@ -70,48 +55,43 @@ public class ImageManager {
     mImage2Url.put(loadingImageView, url);
     mUrl2Image.put(url, loadingImageView);
     if (mLruCache.get(url) != null) {
-      loadingImageView.setImageBitmap(mLruCache.get(url));
-      mImage2Url.remove(loadingImageView);
-      mUrl2Image.remove(url);
+      bindImage(url);
       return;
     }
-    httpRequest(url);
+    downloadBitmap(url);
   }
 
-  private void httpRequest(String url) {
-    Message message = Message.obtain();
-    message.obj = url;
-    mRequestHandler.sendMessage(message);
-  }
-
-  private void handleRequest(final String url) {
-    final Bitmap bitmap = downloadBitMap(url);
-
-    mLruCache.put(url, bitmap);
-
-    mUiHandler.post(
-        new Runnable() {
-          @Override
-          public void run() {
-            if (mUrl2Image.get(url) != null) {
-              mUrl2Image.get(url).setImageBitmap(bitmap);
-              mImage2Url.remove(mUrl2Image.get(url));
-              mUrl2Image.remove(url);
-            }
-          }
-        });
-  }
-
-  @WorkerThread
-  private Bitmap downloadBitMap(String url) {
+  private void downloadBitmap(final String url) {
     Request request = new Request.Builder().url(url).build();
-    try {
-      Response response = mOkHttpClient.newCall(request).execute();
-      InputStream inputStream = response.body().byteStream();
-      return BitmapFactory.decodeStream(inputStream);
-    } catch (Exception ex) {
-      ex.printStackTrace();
-    }
-    return null;
+    Callback callback =
+        new Callback() {
+          @Override
+          public void onFailure(Call call, IOException e) {
+            e.printStackTrace();
+          }
+
+          @Override
+          public void onResponse(Call call, Response response) {
+            InputStream inputStream = response.body().byteStream();
+            final Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            mLruCache.put(url, bitmap);
+            mUiHandler.post(
+                new Runnable() {
+                  @Override
+                  public void run() {
+                    if (mUrl2Image.get(url) != null) {
+                      bindImage(url);
+                    }
+                  }
+                });
+          }
+        };
+    mOkHttpClient.newCall(request).enqueue(callback);
+  }
+
+  private void bindImage(String url) {
+    mUrl2Image.get(url).setImageBitmap(mLruCache.get(url));
+    mImage2Url.remove(mUrl2Image.get(url));
+    mUrl2Image.remove(url);
   }
 }
